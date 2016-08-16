f18797b50c566740bab02171e29bf102c07d39c6
TingAnLiu <vincent.ta.liu@gmail.com>
2016-08-16 20:57:58 +0800

    add user model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe User, :type => :model do
+  it "can create user" do
+    user = User.create(
+      email: 'user@mail.com',
+      password: '12345678',
+      password_confirmation: '12345678')
+
+    expect(User.find_by(email: 'user@mail.com')).to eq(user)
+    expect(user.valid_password?('12345678')).to be_truthy
+    expect(user.valid_password?('1234')).to be_falsey
+    expect(User.all.length).to eq(1)
+  end
+
+  it "cannot create user if password and password_confirmation not the same" do
+    user = User.create(
+      email: 'user@mail.com',
+      password: '12345678',
+      password_confirmation: '1234')
+
+    expect(User.all.length).to eq(0)
+  end
+end

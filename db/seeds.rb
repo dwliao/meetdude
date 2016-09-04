f95c5fd2295ee0285efbbc7719b316be79a95971
dwliao <dw.liao@hotmail.com>
2016-09-05 01:18:22 +0800

    Many accounts are created by Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "這個種子檔會自動建立30個帳號"
+
+30.times do
+  User.create(
+    email: Faker::Internet.email,
+    name: Faker::Company.name,
+    password: 123456,
+    password_confirmation: 123456
+  )
+end

9376e14d55d5c4c905b0afed98c94c514cea16d7
TingAnLiu <vincent.ta.liu@gmail.com>
2016-09-07 22:33:50 +0800

    Update seed file to create posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-puts "這個種子檔會自動建立30個帳號"
+user_number = 10
+posts_number_per_user = 10
 
-30.times do
-  User.create(
+puts "Create #{user_number} users and #{posts_number_per_user} posts for each user"
+
+user = User.create(
+  email: 'test@mail.com',
+  name: 'test',
+  password: 123456,
+  password_confirmation: 123456)
+
+user_number.times do
+  user = User.create(
     email: Faker::Internet.email,
     name: Faker::Name.name,
     password: 123456,
-    password_confirmation: 123456
-  )
+    password_confirmation: 123456)
+  posts_number_per_user.times do
+    user.posts.create(
+      title: Faker::Hipster.sentence,
+      description: Faker::Hipster.paragraph)
+  end
 end

6270de07fcafd5182d9d8709e16ec69b6e2e1185
TingAnLiu <vincent.ta.liu@gmail.com>
2016-09-24 12:55:44 +0800

    Remove some unused files and rename welcome controller to page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'pages#index'
   get ':id' => 'users#show'
   post 'searchUsers' => 'users#search_users'
   post 'appendPostsByTargetId' => 'posts#append_posts_by_target_id'

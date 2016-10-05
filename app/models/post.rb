62302b5b433082a080afe1f4298dfedeadaaa47d
dwliao <dw.liao@hotmail.com>
2016-10-06 00:48:30 +0800

    Post.target_id shouldn't be null

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,17 @@
 class Post < ActiveRecord::Base
   belongs_to :user, class_name: "User", foreign_key: "user_id"
   belongs_to :target, class_name: "User", foreign_key: "target_id"
+  before_save :check_target_id!
 
   def self.no_description
     where(:description => nil)
+  end
+
+  protected
+
+  def check_target_id!
+    if !self.target_id
+      self.target_id = self.user_id
+    end
   end
 end

5616a97aad6906b48d17d241225106ad3417f766
dwliao <dw.liao@hotmail.com>
2017-05-10 17:31:25 +0800

    Adds destroy action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
                               constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: [:show, :create, :update]
+      resources :users, only: [:show, :create, :update, :destroy]
     end
   end
 
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -94,4 +94,13 @@ RSpec.describe Api::V1::UsersController do
       it { expect(response).to have_http_status 422}
     end
   end
+
+  describe "DELETE destroy" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      delete :destroy, { id: @user.id }, format: :json
+    end
+
+    it { expect(response).to have_http_status 204 }
+  end
 end

c4e936863d3aebad03490762dfb3de68d7f4f3ab
dwliao <dw.liao@hotmail.com>
2017-05-10 17:23:35 +0800

    Adds update action the users controllers

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -54,4 +54,44 @@ RSpec.describe Api::V1::UsersController do
       it { expect(response).to have_http_status 422 }
     end
   end
+
+  describe "PUT/PATCH #update" do
+
+    context "when is successfully updated" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id,
+                         user: { email: "newmail@example.com",
+                                 name: "John" } }, format: :json
+      end
+
+      it "renders the json representation for the updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq "newmail@example.com"
+        expect(user_response[:name]).to eq "John"
+      end
+
+      it { expect(response).to have_http_status 200}
+    end
+
+    context "when is not created" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id,
+                         user: { email: "bademail.com" } }, format: :json
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the user could not be updated" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+
+      it { expect(response).to have_http_status 422}
+    end
+  end
 end

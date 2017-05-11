f3285e9c528b774bb21d16db35aef478976b3fa3
dwliao <dw.liao@hotmail.com>
2017-05-11 16:18:09 +0800

    Adds authenticable module for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  include Authenticable
+
   def after_sign_in_path_for(resource)
     "/" + current_user.id.to_s
   end
---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,6 @@
+module Authenticable
+  
+  def current_user
+    @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
+  end
+end

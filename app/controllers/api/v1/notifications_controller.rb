a84c86802a61cad275933977774a18755c26f94e
廖昱維 <YuWei@liaoweishende-MacBook-Pro.local>
2017-05-24 12:19:35 +0800

    Fine tune json output for notifications

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -3,12 +3,20 @@ class Api::V1::NotificationsController < ApplicationController
 
   def index
     @notifications = current_user.notifications
-    render json: serialize_as_json(@notifications), status: 200
+    render json: serialize_json_for(@notifications), status: 200
   end
 
   def show
     @notification = current_user.notifications.find(params[:id])
     @notification.have_read
-    render json: serialize_as_json(@notification), status: 200
+    render json: serialize_json_for(@notification), status: 200
+  end
+
+  private
+
+  def serialize_json_for(notification)
+    notification.as_json(include: { user: { only: [:email, :name] },
+                                  notified_by: { only: :name },
+                                  post: { only: :description } })
   end
 end
